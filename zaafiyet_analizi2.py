7749bb51bc42a911a52f0038b8c9a327508a0edc
sgdeep43 <48888432+sgdeep43@users.noreply.github.com>
2020-11-23 02:35:08 +0300

    Add files via upload

---
+++ zaafiyet_analizi2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+
+os.system("apt-get install figlet")
+os.system("clear")
+os.system("figlet ZAAFIYET ANALIZI")
+print("""
+
+Hos Geldiniz Arac Baslatiliyor..
+
+
+	""")
+os.system("lynis audit system")
+
+print("""
+
+Aciklama Bolumu
+	""")

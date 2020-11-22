53fce7607f9114edd5d912d64ef67c709fc6a968
sgdeep43 <48888432+sgdeep43@users.noreply.github.com>
2020-11-23 01:39:58 +0300

    Add files via upload

---
+++ rootkit_tarama.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import os
+
+os.system("apt-get install figlet")
+os.system("apt-get install chkrootkit -y")
+os.system("clear")
+os.system("figlet ROOTKIT TARAMA")
+print("""
+
+Rootkit Tarama Aracina hosgeldiniz
+
+""")
+
+os.system("chkrootkit")
+
